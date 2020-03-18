e8d07d578d6e3e2d6f6501666da707b3f721578e
Matthew Harrigan <matthew.harrigan@bbc.co.uk>
2020-03-18 20:47:08 +0000

    Adding cli

--- cli.js
+++ cli.js
@@ -1,7 +1,52 @@
-#!/usr/bin/env node
-var argv = require('yargs')
-    .usage('Usage: $0 -start [date] -end [date]')
-    .demandOption(['start','end'])
-    .argv;
+const createHTTPClient = require("./httpClient");
+const getISOWeek = require("date-fns/get_iso_week");
+const eachDay = require("date-fns/each_day");
+const isWeekend = require("date-fns/is_weekend");
+const format = require("date-fns/format");
+const fs = require("fs");
 
-console.log('The start/end  ', argv.start, argv.end);
+const inquirer = require('inquirer');
+inquirer.registerPrompt('datetime', require('inquirer-datepicker-prompt'))
+
+inquirer
+  .prompt([
+    {
+      type: 'datetime',
+      name: 'start',
+      message: 'Start date',
+      format: ['d', '/', 'm', '/', 'yyyy']
+    }
+  ])
+  .then(answers => {
+
+    const { chargeCode, staff, url } = require("./config.json");
+
+    const isoStartDateWithoutMS = answers.start.toISOString().slice(0,-5)+"Z";
+    const urlWithUserStartDate = url.replace(/START/g, encodeURI(isoStartDateWithoutMS));
+
+    const getName = event => event.invitees[0].displayName;
+    const getStaffNumber = event => staff[getName(event)];
+    const getDate = day => format(day, "DD/MM/YYYY");
+    
+    const rowTemplate = (day, event) =>
+      `,${getStaffNumber(event)},${getName(event)},${chargeCode},${getISOWeek(
+        day
+      )},On-call: ${getDate(day)},,,,,,,,,,,,,${+!isWeekend(day)},${+isWeekend(
+        day
+      )}`;
+    
+    const httpClient = createHTTPClient();
+
+    (async () => {
+      const { events } = await httpClient.get(urlWithUserStartDate);
+    
+      const out = events
+        .flatMap(event =>
+          eachDay(event.start, event.end).map(day => rowTemplate(day, event))
+        )
+        .join("\n");
+    
+      console.log(out);
+    })();
+
+  });
