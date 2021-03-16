5f5b4522fd7359beb6d4a4bb950344261e954d67
Matthew Harrigan <matthew.harrigan@bbc.co.uk>
2021-03-16 20:51:23 +0000

    Removing excel

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const isWeekend = require("date-fns/is_weekend");
 const format = require("date-fns/format");
 const fs = require("fs");
 
-var Excel = require("exceljs");
-
 const inquirer = require("inquirer");
 inquirer.registerPrompt("datetime", require("inquirer-datepicker-prompt"));
 
@@ -81,59 +79,6 @@ inquirer
         }, {});
 
       console.log(names);
-
-      // events.flatMap((event) => {
-      //   eachDay(event.start, event.end).map((day) => {
-      //     console.log(day);
-      //     console.log(event);
-      //     rowTemplate(day, event);
-      //   });
-      // });
-
-      async function excelOp() {
-        const { events } = await httpClient.get(urlWithUserEndDate);
-        let workbook = new Excel.Workbook();
-        workbook = await workbook.xlsx.readFile("SummaryTimesheet.xlsx");
-        let worksheet = workbook.getWorksheet("Summary Timesheet");
-
-        const date = new Date();
-        const thisMonthString = date.toLocaleString('default', { month: 'long' });
-        const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
-        const lastMonthString = lastMonth.toLocaleString('default', { month: 'long' });
-
-        worksheet.getRow(9).getCell('B').value = `${lastMonthString} - ${thisMonthString}`;
-
-        // const today = date.format("DD/MM/YYYY");
-        // today.getFullYear(); 
-        // today.getMonth();
-        // today.getDate();
-
-
-
-        // worksheet.getRow(9).getCell('B').value = today;
-
-        let count = 12;
-
-        events.flatMap((event) => {
-          eachDay(event.start, event.end).map((day) => {
-            worksheet.getRow(count).getCell('B').value = getStaffNumber(event);
-            worksheet.getRow(count).getCell('C').value = getName(event);
-            worksheet.getRow(count).getCell('D').value = chargeCode;
-            worksheet.getRow(count).getCell('E').value = getISOWeek(
-              day
-            )
-            worksheet.getRow(count).getCell('F').value = `On-call: ${getDate(day)}`;
-            worksheet.getRow(count).getCell('S').value = +!isWeekend(day);
-            worksheet.getRow(count).getCell('T').value = +isWeekend(day);
-
-
-            count += 1;
-          });
-        });
-
-        // workbook.xlsx.writeFile(`SummaryTimesheet-${team}-${lastMonthString}-${thisMonthString}-${date.getFullYear()}.xlsx`);
-      }
-
-      excelOp();
+      
     })();
   });
