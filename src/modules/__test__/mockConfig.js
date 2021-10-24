95013bce6f2d5be1a9b7199c492566b60463caca
Matthew Harrigan <matthew.harrigan@bbc.co.uk>
2021-10-24 13:40:59 +0100

    Changing test data

--- src/modules/__test__/mockConfig.js
+++ src/modules/__test__/mockConfig.js
@@ -5,28 +5,28 @@ const mockConfig = {
       staff: [
         {
           displayName: "Todd Bonzalez",
-          number: "524870J",
+          number: "555555A",
           division: "england-and-wales",
         },
         {
           displayName: "Willie Dustice",
-          number: "506352B",
+          number: "555555C",
           division: "england-and-wales",
         },
         {
           displayName: "Bobson Dugnutt",
-          number: "527770W",
+          number: "555555B",
           division: "england-and-wales",
         },
         {
           displayName: "dorse.o.hintline",
-          number: "553398E",
+          number: "555555D",
           division: "england-and-wales",
         },
       ],
       calendarPage:
         "https://confluence.dev.bbc.co.uk/pages/viewpage.action?pageId=123456789",
-      costCentre: "S3823",
+      costCentre: "S1234",
       url: "https://confluence.dev.bbc.co.uk/rest/calendar-services/1.0/calendar/events.json?subCalendarId=950d9966-d3b4-465e-aae2-d2f9cc023e42&userTimeZoneId=UTC&start=START&end=END",
     },
   ],
